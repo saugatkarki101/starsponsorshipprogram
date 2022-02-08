105be35195d596645fea728e198a750b95168b97
jpdsnz <57234105+jpdsnz@users.noreply.github.com>
2022-02-08 07:31:30 -0600

    Star Web App 2nd Draft
    Updated this with some interactivity, animation and ux tweaks. Replaced links in nav with the ones on their current site.  I created the blog/news pages in bare form to be connected to the db. Login page is included and accessable from admin button. I also created all the blank pages needed to make it easier to finish. Everything is linked in the navbar though I have the buttons disabled right now. There is still plenty of things to fix and tweak, such as the footer for example and the remaining pages. This front end may have to be changed also if they do not agree with it, but now we have some barebones markup/css code to go off of.

---
+++ Star Website First draft/js/elementFadeInFadeOut.js
@@ -0,0 +1,18 @@
+//JQuery 3.5.1 Elements Fade in on scroll down/ Fade out on scroll up
+//------- This uses the .hideme class ------//
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".hideme").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
