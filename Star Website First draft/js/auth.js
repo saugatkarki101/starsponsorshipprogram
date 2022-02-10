55e5b6f0487ac01075dee8f356a4d092b0d1ff64
Bishesh Pote <bisheshpote@Bisheshs-MBP.lan>
2022-02-09 19:25:15 -0600

    login logout checkStatus

--- Star Website First draft/js/auth.js
+++ Star Website First draft/js/auth.js
@@ -1,5 +1,7 @@
-import {auth, createUserWithEmailAndPassword} from './firebase.js';
+import {auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged} from './firebase.js';
 
+
+// Log In
 const signInForm = document.querySelector('#form-signin');
 signInForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -11,8 +13,36 @@ signInForm.addEventListener('submit', (e) => {
   console.log(email); 
   console.log(password);
   // sign up the user
-  createUserWithEmailAndPassword(auth, email, password)
+  signInWithEmailAndPassword (auth, email, password)
   .then((userCredential) => {
-    const user = userCredential.user;
+
+        const user = userCredential.user;
+        console.log(user);
+        signInForm.reset();
   })
+  .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+  });
+});
+
+// Log Out
+const logOut = document.querySelector('#logout');
+logOut.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+ })
+
+
+ // Check User Status
+onAuthStateChanged(auth, (user) => {
+    if(user) {
+        const uid = user.uid;
+        console.log("Check Status: User signed in.");
+    }
+    else {
+        console.log("Check Status: User logged out.");
+    }
 });
--- Star Website First draft/js/firebase.js
+++ Star Website First draft/js/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,5 +25,5 @@ const analytics = getAnalytics(app);
 const auth = getAuth();
 
 export {
-    auth, createUserWithEmailAndPassword 
+    auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged 
 }
