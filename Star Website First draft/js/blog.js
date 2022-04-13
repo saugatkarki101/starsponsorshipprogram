6ed212b38bff57ff9a68d907f81a1f3d6c1e636a
jpdsnz <57234105+jpdsnz@users.noreply.github.com>
2022-04-13 06:04:41 -0500

    Update blog.js

--- Star Website First draft/js/blog.js
+++ Star Website First draft/js/blog.js
@@ -246,7 +246,7 @@ const createCards = (node) => {
     <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" id="featuredBlog">
     <div class="col-auto d-none d-lg-block " >
     <!--<svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-  <img src="${node.image}"   height="250" width="540" object-fit="cover" "
+  <img src="${node.image}"   height="250" width="540" style ="object-fit:cover" "
     alt=""/> </div><div class="col p-4 d-flex flex-column position-static " id="featured" m-0 >
       <h3 class="mb-0">${node.name.substring(0, 100)}</h3>
       <div class="mb-1 text-muted">${months[node.month-1]+' '+node.day+', '+node.year}</div>
